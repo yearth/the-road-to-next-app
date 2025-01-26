ffb9975982722a5c4515bbc7015435486dce664e
Yearthmain <yearthmain@gmail.com>
2025-01-27 00:23:30 +0800

    Optimize the Placeholder component: add optional props icon and set default value for it

--- src/components/placeholder.tsx
+++ src/components/placeholder.tsx
@@ -1,10 +1,20 @@
+import { LucideMessageSquareWarning } from "lucide-react";
+import { cloneElement } from "react";
+
 type PlaceholderProps = {
   label: string;
+  icon?: React.ReactElement;
 };
 
-export function Placeholder({ label }: PlaceholderProps) {
+export function Placeholder({
+  label,
+  icon = <LucideMessageSquareWarning />,
+}: PlaceholderProps) {
   return (
-    <div className="flex-1 self-center flex flex-col justify-center items-center">
+    <div className="flex-1 self-center flex flex-col justify-center items-center gap-y-2">
+      {cloneElement(icon as React.ReactElement<any>, {
+        className: "h-16 w-16",
+      })}
       <h2 className="text-lg text-center">{label}</h2>
     </div>
   );
