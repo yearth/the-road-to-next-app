1fd5a4a08515dfc3dc3f54ecad61285037e2ae2d
Yearthmain <yearthmain@gmail.com>
2025-01-26 23:54:23 +0800

    Optimize the Heading component: make secription optional and add consistent internal padding

--- src/components/heading.tsx
+++ src/components/heading.tsx
@@ -2,25 +2,27 @@ import { Separator } from "./ui/separator";
 
 type HeadingProps = {
   title: string;
-  description: string;
+  description?: string;
 };
 
 export function Heading({ title, description }: HeadingProps) {
   return (
-    <div>
-      <div>
+    <>
+      <div className="px-8">
         <h2
           tabIndex={0}
           className="text-2xl sm:text-3xl font-bold tracking-tight"
         >
           {title}
         </h2>
-        <p className="text-xs sm:text-sm text-muted-foreground">
-          {description}
-        </p>
+        {description && (
+          <p className="text-xs sm:text-sm text-muted-foreground">
+            {description}
+          </p>
+        )}
       </div>
 
       <Separator />
-    </div>
+    </>
   );
 }
